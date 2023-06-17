95229f4c0aca2fd4dd40040c6a0ce37cec343ea4
Ishbenhuben <gmarksantos@gmail.com>
2023-06-17 23:03:42 +0800

    randomized flowers

--- Scenes/GridManager.gd
+++ Scenes/GridManager.gd
@@ -3,8 +3,10 @@ extends Node2D
 const TILE = preload("res://Scenes/Tile.tscn")
 
 var grid : Array
+var flower_deck : Array
 
 func _ready():
+	randomize()
 	grid = init_grid(4)
 
 func init_grid(grid_length:int) -> Array:
@@ -16,7 +18,20 @@ func init_grid(grid_length:int) -> Array:
 		for r in range(grid_length):
 			new_tile = TILE.instantiate()
 			add_child(new_tile)
-			new_tile.init(Vector2(c,r), get_viewport_rect())
+			new_tile.init(Vector2(c,r), get_viewport_rect(), draw_flower_deck())
 			row.append(new_tile)
 		grid.append(row)
 	return new_grid
+
+func reset_flower_deck() -> void:
+	var dupes = 2
+	flower_deck = []
+	for i in range(3):
+		for j in range(dupes):
+			flower_deck.append(i)
+	flower_deck.shuffle()
+
+func draw_flower_deck() -> int:
+	if flower_deck == []:
+		reset_flower_deck()
+	return flower_deck.pop_back()
--- Scenes/Tile.gd
+++ Scenes/Tile.gd
@@ -7,9 +7,10 @@ var tile_coord : Vector2
 func _ready():
 	pass # Replace with function body.
 
-func init(xy:Vector2, _vp:Rect2) -> void:
+func init(xy:Vector2, _vp:Rect2, flower_id:int) -> void:
 	tile_coord = xy
 	set_position(tile_coord * TILE_SIZE)
+	$Flower.set_flower_id(flower_id)
 
 func _on_tile_area_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:

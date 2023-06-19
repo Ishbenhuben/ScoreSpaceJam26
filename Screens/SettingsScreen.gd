36078894be47b900b1f8cab8a873ef5c7f77670b
Jay-ai Locsin <jayailocsin@gmail.com>
2023-06-20 01:36:00 +0800

    sound settings

--- Screens/SettingsScreen.gd
+++ Screens/SettingsScreen.gd
@@ -6,3 +6,11 @@ func _ready():
 
 func _on_back_to_menu_pressed():
 	get_tree().change_scene_to_file("res://Screens/MainMenuScreen.tscn")
+
+
+func _on_music_slider_value_changed(value):
+	SoundManager.music_player.volume_db = value
+
+
+func _on_sound_slider_value_changed(value):
+	SoundManager.fx_player.volume_db = value
